5fe311ff14a6b69f557460496d9503e85f5f2f43
hummbertwo <65779926+hummbertwo@users.noreply.github.com>
2024-12-12 23:05:17 -0600

    Add files via upload

---
+++ js/funtions.js
@@ -0,0 +1,46 @@
+const bars = document.getElementById("bars")
+const audio = document.getElementById("audio")
+
+bars.addEventListener("click", () => {
+  if (audio.muted) {
+    audio.muted = false
+    bars.classList.remove("muted")
+  } else {
+    audio.muted = true
+    bars.classList.add("muted")
+  }
+})
+
+// Crear nieve
+const createSnowflake = () => {
+  const snowflake = document.createElement("div")
+  snowflake.classList.add("snowflake")
+  snowflake.style.left = Math.random() * 100 + "vw"
+  snowflake.style.fontSize = Math.random() * 20 + 10 + "px"
+  snowflake.innerHTML = "&#10052;"
+  document.body.appendChild(snowflake)
+
+  setTimeout(() => {
+    snowflake.remove()
+  }, 10000)
+}
+
+setInterval(createSnowflake, 200)
+
+// Descargar PDF
+ function downloadPDF() {
+            // URL del archivo PDF
+            const pdfUrl = "assets/cv2.pdf"; // Cambia esto por la ruta real del archivo PDF
+
+            // Crear un enlace oculto
+            const link = document.createElement("a");
+            link.href = pdfUrl;
+            link.download = "cv2.pdf"; // Nombre sugerido para la descarga
+
+            // Agregar el enlace al documento y disparar la descarga
+            document.body.appendChild(link);
+            link.click();
+
+            // Eliminar el enlace después de la descarga
+            document.body.removeChild(link);
+}
