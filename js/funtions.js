4f2f3b0adec11ac6f76580ed596a7e13fee50844
hummbertwo <65779926+hummbertwo@users.noreply.github.com>
2024-12-13 15:25:31 -0600

    Update funtions.js

--- js/funtions.js
+++ js/funtions.js
@@ -13,29 +13,29 @@ bars.addEventListener("click", () => {
 
 // Crear nieve
 const createSnowflake = () => {
-  const snowflake = document.createElement("div")
-  snowflake.classList.add("snowflake")
-  snowflake.style.left = Math.random() * 100 + "vw"
-  snowflake.style.fontSize = Math.random() * 20 + 10 + "px"
-  snowflake.innerHTML = "&#10052;"
-  document.body.appendChild(snowflake)
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    snowflake.style.left = Math.random() * 100 + 'vw';
+    snowflake.style.fontSize = Math.random() * 20 + 10 + 'px';
+    snowflake.innerHTML = '&#10052;';
+    document.body.appendChild(snowflake);
 
-  setTimeout(() => {
-    snowflake.remove()
-  }, 10000)
-}
+    setTimeout(() => {
+      snowflake.remove();
+    }, 10000);
+  };
 
-setInterval(createSnowflake, 200)
+  setInterval(createSnowflake, 200);
 
 // Descargar PDF
- function downloadPDF() {
+ function downloadPDF1() {
             // URL del archivo PDF
-            const pdfUrl = "assets/cv2.pdf"; // Cambia esto por la ruta real del archivo PDF
+            const pdfUrl = "descargas/pro1.pdf"; // Cambia esto por la ruta real del archivo PDF
 
             // Crear un enlace oculto
             const link = document.createElement("a");
             link.href = pdfUrl;
-            link.download = "cv2.pdf"; // Nombre sugerido para la descarga
+            link.download = "pro1.pdf"; // Nombre sugerido para la descarga
 
             // Agregar el enlace al documento y disparar la descarga
             document.body.appendChild(link);
@@ -43,4 +43,21 @@ setInterval(createSnowflake, 200)
 
             // Eliminar el enlace después de la descarga
             document.body.removeChild(link);
+}
+
+function downloadPDF2() {
+    // URL del archivo PDF
+    const pdfUrl = "descargas/pro2.pdf"; // Cambia esto por la ruta real del archivo PDF
+
+    // Crear un enlace oculto
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "pro2.pdf"; // Nombre sugerido para la descarga
+
+    // Agregar el enlace al documento y disparar la descarga
+    document.body.appendChild(link);
+    link.click();
+
+    // Eliminar el enlace después de la descarga
+    document.body.removeChild(link);
 }
